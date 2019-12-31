7d417b1d3c8eefe6d3b8070e4bd57e5625a37f4e
liang612 <liang612@126.com>
2019-12-31 15:55:11 +0800

    springboot项目mybatis配置文件

--- src/main/java/com/bryan/dao/EmplMapper.java
+++ src/main/java/com/bryan/dao/EmplMapper.java
@@ -1,15 +1,22 @@
 package com.bryan.dao;
 
 import com.bryan.domin.Employee;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EmplDao {
 	//查询所有员工
 	List<Employee> findAll();
 
+	//根据员工编号查找
+	Employee findByNum(Integer emplNum);
+
 	//添加员工
 	void save(Employee employee);
+
+	//修改员工信息
+	void update(Employee employee);
+
+	//删除员工
+	void delete(Integer emplNum);
 }
